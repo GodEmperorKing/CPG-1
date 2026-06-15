ada7ac81b36d5f00af059c62ea43ad302873607c
The Boss <youralltoeasy@gmail.com>
2026-06-14 22:43:33 -0700

    feat: add interactive greeting.py to Lab 1

---
+++ greeting.py
@@ -0,0 +1,6 @@
+### Simple greeting.py - for CPG-1 Capstone Lab-1
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+
+print(f"Hello, {name}! That is a great name.")
+print(f"I love the color {color} too! It's very cloud-native.")
